a9b7f142a64bce4881980bfe7364d52ee9647d1b
adetiamarhadi <adetia.marhadi@gmail.com>
2020-06-13 16:49:09 +0700

    Chapter 30 : HTTP POST with SpringMVC

--- src/main/java/com/github/adetiamarhadi/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/github/adetiamarhadi/msscbrewery/web/controller/BeerController.java
@@ -2,10 +2,12 @@ package com.github.adetiamarhadi.msscbrewery.web.controller;
 
 import com.github.adetiamarhadi.msscbrewery.services.BeerService;
 import com.github.adetiamarhadi.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,15 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(this.beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(BeerDto beerDto) {
+        BeerDto savedDto = this.beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost:8080/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
