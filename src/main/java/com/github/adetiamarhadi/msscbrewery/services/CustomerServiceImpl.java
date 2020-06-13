86fc99976a325822cd8852ec2199f5c021d0b708
adetiamarhadi <adetia.marhadi@gmail.com>
2020-06-14 00:11:24 +0700

    Assignment 2 - HTTP Operations with Spring MVC

--- src/main/java/com/github/adetiamarhadi/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/com/github/adetiamarhadi/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.github.adetiamarhadi.msscbrewery.services;
 
 import com.github.adetiamarhadi.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -13,5 +15,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .customerId(customerId)
                 .customerName("Adetia Marhadi")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .customerId(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        log.info("updating customer by id {}, to new value {}", customerId.toString(), customerDto.toString());
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.info("deleting customer by id {}", customerId.toString());
     }
 }
--- src/main/java/com/github/adetiamarhadi/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/github/adetiamarhadi/msscbrewery/web/controller/CustomerController.java
@@ -2,11 +2,17 @@ package com.github.adetiamarhadi.msscbrewery.web.controller;
 
 import com.github.adetiamarhadi.msscbrewery.services.CustomerService;
 import com.github.adetiamarhadi.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -24,5 +30,27 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID customerId) {
         return new ResponseEntity<>(this.customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedCustomerDto = this.customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "http://localhost:8080/api/v1/customer/" + savedCustomerDto.getCustomerId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        this.customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+        this.customerService.deleteCustomerById(customerId);
     }
 }
